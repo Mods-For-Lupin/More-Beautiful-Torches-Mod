c2c988f02661b104c9642580dda6d977844be421
jason13official <jason13official@gmail.com>
2026-08-02 13:19:30 -0400

    new recipe

--- neoforge/src/main/java/io/github/jason13official/more_beautiful_torches/datagen/server/MBTRecipeProvider.java
+++ neoforge/src/main/java/io/github/jason13official/more_beautiful_torches/datagen/server/MBTRecipeProvider.java
@@ -8,6 +8,7 @@ import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Block;
@@ -22,21 +23,26 @@ public class MBTRecipeProvider extends RecipeProvider {
   @Override
   protected void buildRecipes(RecipeOutput recipeOutput) {
     for (ModBlocks.TorchEntry entry : ModBlocks.TORCHES) {
-      torch(recipeOutput, entry.standing(), entry.source(), Ingredient.of(Items.STICK));
+      torch(recipeOutput, entry.standing(), entry.source(),
+          Ingredient.of(Items.STICK), Ingredient.of(ItemTags.COALS));
     }
     for (ModBlocks.TorchEntry entry : ModBlocks.SOUL_TORCHES) {
-      torch(recipeOutput, entry.standing(), entry.source(), Ingredient.of(Blocks.SOUL_SOIL, Blocks.SOUL_SAND));
+      torch(recipeOutput, entry.standing(), entry.source(),
+          Ingredient.of(Items.STICK), Ingredient.of(ItemTags.COALS), Ingredient.of(Blocks.SOUL_SOIL, Blocks.SOUL_SAND));
     }
     for (ModBlocks.TorchEntry entry : ModBlocks.REDSTONE_TORCHES) {
-      torch(recipeOutput, entry.standing(), entry.source(), Ingredient.of(Items.REDSTONE));
+      torch(recipeOutput, entry.standing(), entry.source(),
+          Ingredient.of(Items.STICK), Ingredient.of(Items.REDSTONE));
     }
   }
 
-  private void torch(RecipeOutput recipeOutput, Block standing, Block source, Ingredient catalyst) {
-    ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, standing, 4)
-        .requires(source)
-        .requires(catalyst)
-        .unlockedBy(getHasName(source), has(source))
+  private void torch(RecipeOutput recipeOutput, Block standing, Block source, Ingredient... ingredients) {
+    ShapelessRecipeBuilder builder = ShapelessRecipeBuilder.shapeless(RecipeCategory.DECORATIONS, standing, 4)
+        .requires(source);
+    for (Ingredient ingredient : ingredients) {
+      builder.requires(ingredient);
+    }
+    builder.unlockedBy(getHasName(source), has(source))
         .save(recipeOutput);
   }
 }
